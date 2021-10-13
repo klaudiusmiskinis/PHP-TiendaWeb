6daf4570228683dcfe62311d9a3b8884ef9deafe
Klau <klaudiusmiskinis@gmail.com>
2021-10-13 20:03:21 +0200

    Dividiendo el HEAD-LINK del mismo archivo a distintos

--- vistas/componentes/head-link.php
+++ vistas/componentes/head-link.php
@@ -4,9 +4,5 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="../assets/img/icono.png">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../assets/estilos/style-general.css">
-    <link rel="stylesheet" href="../assets/estilos/login.css">
     <title><?php echo 'Tienda - Web' ?></title>
 </head>
