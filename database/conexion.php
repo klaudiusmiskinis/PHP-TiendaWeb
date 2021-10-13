aca194a476a155f727ee050520d04e6a7462bf58
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-13 23:00:20 +0200

    Editando la conexión a la base de datos. (OK)

---
+++ database/conexion.php
@@ -0,0 +1,11 @@
+<?php
+    $servidor = 'localhost';
+    $usuario = 'root';
+    $password = '';
+    $bd = 'tienda';
+    $con = mysqli_connect($servidor,$usuario,$password,$bd);
+
+    if ($con->connect_error) {
+        die("Conexión fallida: " . $con->connect_error);
+    }
+?>
