454145bc9f19b3c5fb9588731e93cd75811e4b41
Klau <klaudiusmiskinis@gmail.com>
2021-10-13 19:46:29 +0200

    Añadiendo fondo con movimiento con CSS

--- vistas/login.php
+++ vistas/login.php
@@ -1,4 +1,12 @@
 <?php include('./componentes/head-link.php')?>
+
+<div class="background">
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+</div>
+
 <body>
     <div class="container-fluid">
         <div class="row justify-content-center mt-5">
@@ -7,8 +15,8 @@
                     <form class="card m-3 p-5">
                         <h1>Login</h1>
                         <p class="text-muted">¡Introduce tu nombre de usuario y contraseña!</p> 
-                        <input class="mt-2 shadow-sm form-control" type="text" name="nombre" placeholder="Nombre de usuario"> 
-                        <input class="mt-3 shadow-sm form-control" type="password" name="password" placeholder="Contraseña"> 
+                        <input class="mt-2 shadow-sm form-control" type="text" name="nombre" placeholder="Nombre de usuario" autocomplete="off" required> 
+                        <input class="mt-3 shadow-sm form-control" type="password" name="password" placeholder="Contraseña" autocomplete="off" required> 
                         <input class="mt-3 shadow-sm text-center btn btn-secondary" type="submit" value="Login" href="#">
                         <div class="col-md-12">
                             <ul class="social-network social-circle">
