5f8aa4c37b1a6bf6dc69c6d3552479afe797ed29
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-13 23:03:35 +0200

    Eliminando head, link y nav-bar

--- vistas/componentes/link.php
+++
@@ -1,9 +0,0 @@
-<?php function componenteLink() { ?>
-    <link rel="icon" href="../assets/img/icono.png">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../assets/estilos/style-general.css">
-    <link rel="stylesheet" href="../assets/estilos/clases.css">
-    <link rel="stylesheet" href="../assets/estilos/login.css">
-    <link rel="stylesheet" href="../assets/estilos/fondo.css">
-    <link rel="stylesheet" href="../assets/estilos/var.css">
-<?php } ?>
