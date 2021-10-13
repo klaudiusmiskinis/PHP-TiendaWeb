851943106d0e47aaf67b46e77a5333b0db0bd16b
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-13 22:57:47 +0200

    Juntando los links en el <head>

---
+++ componentes/head.php
@@ -0,0 +1,17 @@
+<?php function componenteHead($titulo) { ?>
+    <!DOCTYPE html>
+    <html lang='es'>
+    <head>
+        <meta charset='UTF-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css'>
+        <link rel='stylesheet' href='../assets/estilos/style-general.css'>
+        <link rel='stylesheet' href='../assets/estilos/clases.css'>
+        <link rel='stylesheet' href='../assets/estilos/login.css'>
+        <link rel='stylesheet' href='../assets/estilos/fondo.css'>
+        <link rel='stylesheet' href='../assets/estilos/var.css'>
+        <link rel='icon' href='../assets/img/icono.png'>
+        <title><?php echo $titulo ?></title>
+    </head>
+<?php } ?>
