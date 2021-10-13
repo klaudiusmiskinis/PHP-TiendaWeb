984b296d1a59d0501dcdb818e0a275e86e2b4c50
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-13 23:03:15 +0200

    Eliminando login.php y background.php

--- vistas/componentes/background.php
+++
@@ -1,8 +0,0 @@
-<?php function componenteBackground() { ?>
-    <div class="background">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-    </div>
-<?php } ?>
