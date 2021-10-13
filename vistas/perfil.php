631801c75c09de8060410cd998e89af7617803ca
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-13 23:03:55 +0200

    Dando formato básico a perfil.

--- vistas/perfil.php
+++ vistas/perfil.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+    include('../database/conexion.php');
+    include('../componentes/head.php');
+    include('../componentes/background.php');
+    componenteHead('Perfil');
+    componenteBackground()
+?>
