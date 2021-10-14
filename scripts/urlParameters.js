4b74ec7073cc23eb98ab972d5f508d224e15f631
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-14 23:06:56 +0200

    Nuevo manejo de errores 'Existe'

--- scripts/urlParameters.js
+++ scripts/urlParameters.js
@@ -1,10 +1,17 @@
-if (document.title == 'Login' || document.title == 'Register') {
-        let error = '<div class="alert alert-dark mt-2" role="alert">' +
-                        '¡Vaya! Datos incorrectos.' +
-                    '</div>'
-        let sPageURL = window.location.search.substring(1);
-        let parametro = sPageURL.split('=')[1];
+if (document.title == 'Login' || document.title == 'Register'){
+        let url = window.location.search.substring(1);
+        let parametro = url.split('=')[1];
         if(parametro == 'incorrecto'){
+            let error = '<div class="alert alert-dark mt-2" role="alert">' +
+                            '¡Vaya! Datos incorrectos.' +
+                        '</div>';
             document.getElementById('frase-login').insertAdjacentHTML('afterend', error);
+        } else if (parametro == 'existe') {
+            $('#container-login').hide();
+            $('#container-register').slideDown("slow");
+            let error = '<div class="alert alert-dark mt-2" role="alert">' +
+                            'El usuario ya existe.' +
+                        '</div>';
+            document.getElementById('frase-register').insertAdjacentHTML('afterend', error);
         }
 }
