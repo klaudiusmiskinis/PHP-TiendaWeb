a88027cd564b8dabec340ceeaf2eb9008579d905
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-14 23:46:52 +0200

    Editando archivo de BD

--- database/tienda.sql
+++ database/tienda.sql
@@ -1,6 +1,31 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 14-10-2021 a las 23:09:53
+-- Versión del servidor: 10.4.19-MariaDB
+-- Versión de PHP: 8.0.6
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `tienda`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
 
 CREATE TABLE `usuario` (
   `id` int(10) NOT NULL COMMENT 'Id único por usuario.',
@@ -19,7 +44,10 @@ INSERT INTO `usuario` (`id`, `nombre`, `email`, `password`, `avatar`, `rol`) VAL
 (1, 'Klaudius', 'klaudius@email.com', '1234', 'none', 'admin'),
 (2, 'prueba', 'prueba@email.com', '1234', 'none', 'admin'),
 (3, 'user', 'user@email.com', '1234', 'none', 'user'),
-(4, 'admin', 'admin@email.com', '1234', 'none', 'admin');
+(4, 'admin', 'admin@email.com', '1234', 'none', 'admin'),
+(9, 'asd@asd2', 'asd@asd2', 'asd', 'none', 'user'),
+(10, 'klauPrueba', 'a@a', 'a', 'none', 'user'),
+(11, 'asd', 'asd@asd', 'asd', 'none', 'user');
 
 --
 -- Índices para tablas volcadas
@@ -29,8 +57,8 @@ INSERT INTO `usuario` (`id`, `nombre`, `email`, `password`, `avatar`, `rol`) VAL
 -- Indices de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id` (`id`);
+ADD PRIMARY KEY (`id`),
+ADD UNIQUE KEY `id` (`id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -40,7 +68,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'Id único por usuario.', AUTO_INCREMENT=5;
+MODIFY `id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'Id único por usuario.', AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
