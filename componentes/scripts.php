5ff4e774d8ec5844717de818ac2789d4e5e5cc83
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-14 16:01:25 +0200

    Añadiendo componente Scripts

---
+++ componentes/scripts.php
@@ -0,0 +1,5 @@
+<?php function componenteScripts() { ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="../scripts/animaciones.js"></script>
+<?php } ?>
