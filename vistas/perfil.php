dfae9b9591265231950bcf7be1f261edefb4b468
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-14 23:07:46 +0200

    Pagina perfil editada minimamente para datos

--- vistas/perfil.php
+++ vistas/perfil.php
@@ -1,4 +1,5 @@
 <?php
+    session_start();
     include('../database/conexion.php');
     include('../componentes/head.php');
     include('../componentes/background.php');
@@ -10,10 +11,13 @@
     <div class="container-md sticky">
         <div class="row">
             <div class="col-sm-auto navbar navbar-light bg-light mx-5 mt-3 p-2 blur">
-            <p class="m-2">ASD</p>            
+                <p class="m-2"><?php echo 'Bienvenido '.$_SESSION['nombre'] ?></p>            
             </div>
             <nav class="col-md navbar navbar-light bg-light mx-5 mt-3 p-2 blur">
-                <div class="m-2">Tienda</div>
+                <div class="m-2">Esto es una pagina temporal</div>
+                <div class="m-2"><?php echo 'Tu id es el: '.$_SESSION['id'] ?></div>
+                <div class="m-2"><?php echo 'Tu email es el: '.$_SESSION['email'] ?></div>
+                <div class="m-2"><?php echo 'Tu rol es el: '.$_SESSION['rol'] ?></div>
             </nav>
         </div>
     </div>
