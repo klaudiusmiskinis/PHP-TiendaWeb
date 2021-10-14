ddecfb73337e795f7fb115810ac716fe669b664d
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-14 16:11:29 +0200

    Añadiendo clases y cambios de texto

--- vistas/index.php
+++ vistas/index.php
@@ -20,7 +20,7 @@
                         <input class='mt-3 shadow-sm form-control' type='password' name='password' placeholder='Contraseña' autocomplete='off' required> 
                         <input class='mt-3 shadow-sm text-center btn' type='submit' value='Iniciar sesión' href='#'>
                         <p class='text-muted mt-2'>¿No tienes una cuenta?</p>
-                        <a type="text" class="btn btn-primary-outline ml-1" id="btn-to-register">¡Registrate aquí!</a>
+                        <a type="text" class="btn-outline-dark" id="btn-to-register">¡Registrate aquí!</a>
                     </form>
                 </div>
             </div>
@@ -33,13 +33,13 @@
                 <div class='text-center'>
                     <form class='card m-3 p-5' method="POST">
                         <h1>Register</h1>
-                        <p class='text-muted'>¡Introduce tu nombre de usuario y contraseña!</p> 
-                        <input class='mt-2 shadow-sm form-control' type='text' name='nombre' placeholder='Introduce tu nombre de usuario' autocomplete='off' required>
-                        <input class='mt-3 shadow-sm form-control' type='email' name='email' placeholder='Introduce tu email' autocomplete='off' required> 
-                        <input class='mt-3 shadow-sm form-control' type='password' name='password' placeholder='Introduce tu contraseña' autocomplete='off' required>
+                        <p class='text-muted'>¡Registrate rellenando los campos!</p> 
+                        <input class='mt-2 shadow-sm form-control' type='text' name='nombre' placeholder='Introduce tu nombre de usuario *' autocomplete='off' required>
+                        <input class='mt-3 shadow-sm form-control' type='email' name='email' placeholder='Introduce tu email *' autocomplete='off' required> 
+                        <input class='mt-3 shadow-sm form-control' type='password' name='password' placeholder='Introduce tu contraseña *' autocomplete='off' required>
                         <input class='mt-3 shadow-sm text-center btn' type='submit' value='Registrarse'>
                         <p class='text-muted mt-2'>¿Ya tienes una cuenta?</p>
-                        <a type="text" class="btn btn-primary-outline ml-1" id="btn-to-login">¡Inicia sesión!</a>
+                        <a type="text" class="btn-outline-dark" id="btn-to-login">¡Inicia sesión!</a>
                     </form>
                 </div>
             </div>
