b91ad662e6717933f946da7997873094033fd076
Klau <klaudiusmiskinis@gmail.com>
2021-10-15 16:31:14 +0200

    Pasando las rutas de CDN a las librerias locales del proyecto

--- componentes/scripts.php
+++ componentes/scripts.php
@@ -1,6 +1,6 @@
 <?php function componenteScripts() { ?>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="../node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="../node_modules/jquery/dist/jquery.min.js"></script>
     <script src="../scripts/animaciones.js"></script>
     <script src="../scripts/urlParameters.js"></script>
 <?php } ?>
