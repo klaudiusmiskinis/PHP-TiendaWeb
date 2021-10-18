8081847fbd6f7de438dab017315ad9d3caddebc7
Klau <klaudiusmiskinis@gmail.com>
2021-10-18 19:41:19 +0200

    Editando boceto

--- vistas/boceto.php
+++ vistas/boceto.php
@@ -3,12 +3,12 @@
   include('../database/conexion.php');
   include('../componentes/head.php');
   include('../componentes/background.php');
-  componenteHead('Perfil');
+  componenteHead('Boceto');
   componenteBackground();
 ?>
 <body>
-    <div class="container-fluid justify-content-center position-sticky z-index-sticky">
-      <div class="row text-center mx-5">
+    <div class="container-fluid position-sticky z-index-sticky">
+      <div class="row text-center mx-5 mb-0 mt-1">
         <div class="col-sm-auto nav-izq my-1 p-1">
           <a href="#">
             <svg class="profile" xmlns="http://www.w3.org/2000/svg" height="40" fill="white" class="bi bi-person-circle" viewBox="0 0 16 16">
@@ -17,14 +17,25 @@
             </svg>
           </a>
         </div>
-        <div class="col-sm nav-der m-1">
-          <div class="d-flex justify-content-center">
-            <a class="item mt-1 p-2 mx-2">Productos</a>
-            <a class="item mt-1 p-2 mx-2">Tienda</a>
-            <a class="item mt-1 p-2 mx-2">Pedidos</a>
-            <a class="item mt-1 p-2 mx-2">Ofertas</a>
-            <a class="item mt-1 p-2 mx-2">Lorem</a>
-          </div>
+        <div class="col-sm nav my-1 px-3 align-items-center">
+          <a class="px-2 mt-2" href="#">Productos</a>
+        </div>
+        <div class="col-sm nav my-1 px-3 align-items-center">
+          <a class="px-2 mt-2" href="#">Ofertas</a>
+        </div>
+        <div class="col-sm nav my-1 px-3 align-items-center">
+          <a class="px-2" href="#">Outlet</a>
+        </div>
+        <div class="col-sm-auto mr-1 nav-der my-1 px-3 justify-content-center">
+        </div>
+      </div>
+      <div class="row m-0 p-0 justify-content-center">
+        <div class="col-sm-auto bg-light">
+          <svg xmlns="http://www.w3.org/2000/svg" height="20" fill="currentColor" class="bi bi-eye-slash" viewBox="0 0 16 16">
+            <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7.028 7.028 0 0 0-2.79.588l.77.771A5.944 5.944 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.134 13.134 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755-.165.165-.337.328-.517.486l.708.709z"/>
+            <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829l.822.822zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829z"/>
+            <path d="M3.35 5.47c-.18.16-.353.322-.518.487A13.134 13.134 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7.029 7.029 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12-.708.708z"/>
+          </svg>
         </div>
       </div>
     </div>
