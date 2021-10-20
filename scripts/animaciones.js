8bc122eb49cd9e833f0cd3374c2b7b39b4fc9d8e
Klau <klaudiusmiskinis@gmail.com>
2021-10-20 20:08:33 +0200

    Animaciones de navbar

--- scripts/animaciones.js
+++ scripts/animaciones.js
@@ -1,14 +1,18 @@
 $('#container-register').hide();
 $('#container-login').hide();
-$('#container-login').slideDown("slow");
+$('#container-login').slideDown('slow');
 
 //CLICK FUNCIONES
-$("#btn-to-register").on("click", function() {
-    $('#container-login').slideUp("slow");
-    $('#container-register').slideDown("slow");
+$('#btn-to-register').on('click', function() {
+    $('#container-login').slideUp('slow');
+    $('#container-register').slideDown('slow');
 });
 
-$("#btn-to-login").on("click",function() {
-    $('#container-register').slideUp("slow");
-    $('#container-login').slideDown("slow");
+$('#btn-to-login').on('click', function() {
+    $('#container-register').slideUp('slow');
+    $('#container-login').slideDown('slow');
 });
+
+$('#nav-nombre').on('click', function() {
+    $('#nav-nombre').animate("slide", {direction: "left"}, 1000);
+})
