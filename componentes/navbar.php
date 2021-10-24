5b27e50011da945e647c25dc1175f8a4927d1b4b
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-24 11:58:06 +0200

    Editando navbar para generar iconos

--- componentes/navbar.php
+++ componentes/navbar.php
@@ -1,21 +1,23 @@
-<?php function componenteNavbar($nombre) { ?>
+<?php 
+include('../componentes/iconos.php');
+function componenteNavbar($nombre) { ?>
 <?php $nombre = strtok($nombre, ' '); ?>
 
-    <nav class="navbar border-bottom fixed-top" id="navegador">
+    <nav class="navbar fixed-top" id="navegador">
         <div class="container-fluid">
-            <div class="bg-dark text-light">
-                <a class="bg-dark text-light" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBackdrop" aria-controls="offcanvasWithBackdrop">
-                    <img src="../assets/svg/person-circle.svg" width="25" class="svg my-2 mx-1">
+            <div class="bg-dark text-light p-2">
+                <a type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBackdrop" aria-controls="offcanvasWithBackdrop">
+                    <?php generarIconPerfil(); ?>
                 </a>
             </div>
-            <div class="d-flex bg-dark text-light">
+            <div class="d-flex bg-dark text-light p-2">
                 <a class="nav-link" href="#">Productos</a>
                 <a class="nav-link" href="#">Home</a>
                 <a class="nav-link" href="#">Ofertas</a>
             </div>
-            <div class="bg-dark text-light">
+            <div class="bg-dark text-light p-2">
                 <a href="./logout.php">
-                    <img src="../assets/svg/door-closed.svg" width="30" alt="door" class="svg my-2 mx-1 salir">
+                   <?php generarIconLogOut(); ?>
                 </a>
             </div>
         </div>
@@ -24,7 +26,7 @@
     <div class="offcanvas offcanvas-start bg-dark text-light" tabindex="-1" id="offcanvasWithBackdrop" aria-labelledby="offcanvasWithBackdropLabel">
         <div class="offcanvas-header">
             <h5 class="offcanvas-title" id="offcanvasWithBackdropLabel">Panel de usuario</h5>
-            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"></button>
         </div>
         <div class="offcanvas-body">
             <div class="row">
