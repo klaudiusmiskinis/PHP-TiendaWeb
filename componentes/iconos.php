aa55eadfabf5278610d196e61450f0a45176bfb8
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-24 11:57:50 +0200

    Añadiendo componente iconos.php

---
+++ componentes/iconos.php
@@ -0,0 +1,13 @@
+<?php function generarIconLogOut() { ?>
+    <svg xmlns="http://www.w3.org/2000/svg" class="svg my-2 mx-1 salir"  width="30" alt="door" fill="currentColor" class="bi bi-door-closed" viewBox="0 0 16 16">
+        <path d="M3 2a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v13h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V2zm1 13h8V2H4v13z"/>
+        <path d="M9 9a1 1 0 1 0 2 0 1 1 0 0 0-2 0z"/>
+    </svg>
+<?php } ?>
+
+<?php function generarIconPerfil() { ?>
+    <svg xmlns="http://www.w3.org/2000/svg" class="svg my-2 mx-1" width="25" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+    </svg>
+<?php } ?>
