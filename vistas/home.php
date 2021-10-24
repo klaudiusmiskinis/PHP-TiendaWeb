8affdb688f814b20adebe3b8f5a70e46aef081da
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-24 10:46:27 +0200

    Cambiando condicion de comprobación de Session

--- vistas/home.php
+++ vistas/home.php
@@ -1,19 +1,17 @@
 <?php
     session_start();
-    if (!empty($_SESSION['nombre'])) {
-        include('../componentes/background.php');
-        include('../componentes/scripts.php');
-        include('../componentes/navbar.php');
-        include('../database/conexion.php');
-        include('../componentes/head.php');
-        
-        componenteHead('Home');
-        componenteBackground();
-        componenteNavbar($_SESSION['nombre']);
-        componenteScripts();
-    } else {
+    if (empty($_SESSION['nombre'])) {
         header("Location: index.php");
     }
+    include('../componentes/background.php');
+    include('../componentes/scripts.php');
+    include('../componentes/navbar.php');
+    include('../database/conexion.php');
+    include('../componentes/head.php');
+    componenteHead('Home');
+    componenteBackground();
+    componenteNavbar($_SESSION['nombre']);
+    componenteScripts();
    
     
     
