79386307d09d606a47d2273c243b0bc694020db1
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-24 15:56:24 +0200

    Eliminando contenido de perfil.php

--- vistas/perfil.php
+++ vistas/perfil.php
@@ -14,33 +14,5 @@
 
 <body>
     <?php componenteNavbar($_SESSION['nombre']); ?>
-    <div class="container-fluid justify-content-between rounded bg-dark text-light">
-        <div class="d-flex justify-content-between row">
-            <div class="col">
-                <div class="card bg-dark">
-                    <!-- <img src="#" class="card-img-top"> -->
-                    <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col mx-4">
-            <div class="card bg-dark">
-                    <!-- <img src="#" class="card-img-top"> -->
-                    <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card bg-dark">
-                    <!-- <img src="#" class="card-img-top"> -->
-                    <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
     <?php componenteScripts(); ?>
 </body>
