f1fee17ba4df970e5f0e9528783247f3f5944178
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-24 10:46:01 +0200

    Editando navbar y añadiendo panel lateral

--- componentes/navbar.php
+++ componentes/navbar.php
@@ -1,47 +1,32 @@
 <?php function componenteNavbar($nombre) { ?>
 <?php $nombre = strtok($nombre, ' '); ?>
-    <div class="d-flex justify-content-between mb-2" id="navegador">
-        <nav class="navbar m-2 rounded bg-dark">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col text-center">
-                    <a href="./perfil.php">
-                        <img src="../assets/svg/person-circle.svg" width="25" class="svg my-2 mx-1">
-                        </a>
-                    </div>
-                    <div class="col">
-                        <a class="nav-link" id="nav-nombre"><?php echo $nombre; ?></a>
-                    </div>
-                </div>
-            </div>
-        </nav>
 
-        <nav class="navbar m-2 rounded bg-dark">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col">
-                        <a class="nav-link" href="#">Productos</a>
-                    </div>
-                    <div class="col">
-                        <a class="nav-link" href="#">Home</a>
-                    </div>
-                    <div class="col">
-                        <a class="nav-link" href="#">Ofertas</a>
-                    </div>
-                </div>
+    <nav class="navbar border-bottom bg-dark text-light fixed-top">
+        <div class="container-fluid">
+            <a type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBackdrop" aria-controls="offcanvasWithBackdrop">
+                <img src="../assets/svg/person-circle.svg" width="25" class="svg my-2 mx-1">
+            </a>
+            <div class="d-flex">
+                <a class="nav-link" href="#">Productos</a>
+                <a class="nav-link" href="#">Home</a>
+                <a class="nav-link" href="#">Ofertas</a>
             </div>
-        </nav>
+            <div>
+                <a href="./logout.php">
+                    <img src="../assets/svg/door-closed.svg" width="30" alt="door" class="svg my-2 mx-1 salir">
+                </a>
+            </div>
+        </div>
+    </nav>
 
-        <nav class="navbar m-2 rounded bg-dark">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col text-center">
-                    <a href="./logout.php">
-                        <img src="../assets/svg/door-closed.svg" width="30" alt="door" class="svg my-2 mx-1 salir">
-                    </a>
-                    </div>
-                </div>
-            </div>
-        </nav>
+    <div class="offcanvas offcanvas-start" tabindex="-1" id="panel-lateral" aria-labelledby="offcanvasWithBackdropLabel">
+        <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="panel-lateral">Offcanvas with backdrop</h5>
+            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+            <p>.....</p>
+        </div>
     </div>
+
 <?php } ?>
