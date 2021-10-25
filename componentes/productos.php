e6a52677a495581829c6b3110b78bba77ce45f21
Klau <klaudiusmiskinis@gmail.com>
2021-10-25 19:31:40 +0200

    Cambiando componenteProductos() por componenteTest()

--- componentes/productos.php
+++ componentes/productos.php
@@ -1,4 +1,4 @@
-<?php function componenteProductos() { ?>
+<?php function componenteTest() { ?>
     <div class="row mb-2">
         <div class="col bg-dark text-light rounded mr-2 p-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium omnis qui excepturi repellendus, reprehenderit in. Expedita exercitationem sapiente voluptatibus similique repudiandae error ullam ab non iure, corrupti ducimus dolor nemo.</div>
         <div class="col bg-dark text-light rounded mx-2 p-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium omnis qui excepturi repellendus, reprehenderit in. Expedita exercitationem sapiente voluptatibus similique repudiandae error ullam ab non iure, corrupti ducimus dolor nemo.</div>
