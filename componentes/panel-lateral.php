2c21cb84906ab811e67efb1c3c05d7005f1920ca
Klau <klaudiusmiskinis@gmail.com>
2021-10-26 20:10:39 +0200

    Editando estilos de botones del panel

--- componentes/panel-lateral.php
+++ componentes/panel-lateral.php
@@ -5,11 +5,8 @@
 
 <div class="offcanvas offcanvas-start bg-dark text-light" tabindex="-1" id="offcanvasWithBackdrop" aria-labelledby="panel-lateral">
     <div class="offcanvas-header text-center">
-        <button class="btn-close" data-bs-dismiss="offcanvas"></button>
-        <div class="d-grid gap-2">
-            <h5 class="offcanvas-title" id="panel-lateral">Panel de usuario</h5>
-            <small class="text-muted"><?php echo '('.$nombre.')'; ?></small>
-        </div>
+        <h5 class="offcanvas-title" id="panel-lateral">Panel de usuario</h5>
+        <small class="text-muted"><?php echo '('.$nombre.')'; ?></small>
     </div>
     <div class="offcanvas-body">
             <?php if($rol == 'admin') { ?>
@@ -25,13 +22,13 @@
             <!-- USUARIOS -->
             <div class="row text-center">
                 <div class="col d-grid gap-1">
-                    <button class="btn btn-outline-light shadow-sm" type="button" data-bs-toggle="collapse" data-bs-target="#desplegar-usuarios" aria-expanded="false" aria-controls="desplegar-usuarios">· Usuarios ·</button>
+                    <button class="btn btn-light shadow-sm" type="button" data-bs-toggle="collapse" data-bs-target="#desplegar-usuarios" aria-expanded="false" aria-controls="desplegar-usuarios">· Usuarios ·</button>
                 </div>
                 <div class="collapse" id="desplegar-usuarios">
                     <div class="container-fluid d-grid gap-1 mt-1">
-                        <a class="btn btn-secondary shadow-sm">Ver usuarios</a>
-                        <a class="btn btn-secondary shadow-sm">Crear usuario</a>
-                        <a class="btn btn-secondary shadow-sm">Eliminar usuarios</a>
+                        <a class="btn btn-outline-secondary shadow-sm">Ver usuarios</a>
+                        <a class="btn btn-outline-secondary shadow-sm">Crear usuario</a>
+                        <a class="btn btn-outline-secondary shadow-sm">Eliminar usuarios</a>
                         <hr>
                     </div>
                 </div>
@@ -40,13 +37,13 @@
             <!-- PRODUCTOS -->
             <div class="row text-center mt-1">
                 <div class="col d-grid gap-1">
-                    <button class="btn btn-outline-light shadow-sm" type="button" data-bs-toggle="collapse" data-bs-target="#desplegar-productos" aria-expanded="false" aria-controls="desplegar-productos">· Productos ·</button>
+                    <button class="btn btn-light shadow-sm" type="button" data-bs-toggle="collapse" data-bs-target="#desplegar-productos" aria-expanded="false" aria-controls="desplegar-productos">· Productos ·</button>
                 </div>
                 <div class="collapse" id="desplegar-productos">
                     <div class="container-fluid d-grid gap-1 mt-1">
-                        <a class="btn btn-secondary shadow-sm">Ver productos</a>
-                        <a class="btn btn-secondary shadow-sm">Crear productos</a>
-                        <a class="btn btn-secondary shadow-sm">Eliminar productos</a>
+                        <a class="btn btn-outline-secondary shadow-sm">Ver productos</a>
+                        <a class="btn btn-outline-secondary shadow-sm">Crear productos</a>
+                        <a class="btn btn-outline-secondary shadow-sm">Eliminar productos</a>
                         <hr>
                     </div>
                 </div>
@@ -55,13 +52,13 @@
             <!-- CATEGORIAS -->
             <div class="row text-center mt-1">
                 <div class="col d-grid gap-1">
-                    <button class="btn btn-outline-light shadow-sm" type="button" data-bs-toggle="collapse" data-bs-target="#desplegar-categorias" aria-expanded="false" aria-controls="desplegar-categorias">· Categorias ·</button>
+                    <button class="btn btn-light shadow-sm" type="button" data-bs-toggle="collapse" data-bs-target="#desplegar-categorias" aria-expanded="false" aria-controls="desplegar-categorias">· Categorias ·</button>
                 </div>
                 <div class="collapse" id="desplegar-categorias">
                     <div class="container-fluid d-grid gap-1 mt-1">
-                        <a class="btn btn-secondary shadow-sm">Ver categorias</a>
-                        <a class="btn btn-secondary shadow-sm">Crear categorias</a>
-                        <a class="btn btn-secondary shadow-sm">Eliminar categorias</a>
+                        <a class="btn btn btn-outline-secondary shadow-sm">Ver categorias</a>
+                        <a class="btn btn btn-outline-secondary shadow-sm">Crear categorias</a>
+                        <a class="btn btn btn-outline-secondary shadow-sm">Eliminar categorias</a>
                         <hr>
                     </div>
                 </div>
@@ -70,13 +67,13 @@
             <!-- SUBCATEGORIAS -->
             <div class="row text-center mt-1">
                 <div class="col d-grid gap-1">
-                    <button class="btn btn-outline-light shadow-sm" type="button" data-bs-toggle="collapse" data-bs-target="#desplegar-subcategorias" aria-expanded="false" aria-controls="desplegar-categorias">· Subcategorias ·</button>
+                    <button class="btn btn-light shadow-sm" type="button" data-bs-toggle="collapse" data-bs-target="#desplegar-subcategorias" aria-expanded="false" aria-controls="desplegar-categorias">· Subcategorias ·</button>
                 </div>
                 <div class="collapse" id="desplegar-subcategorias">
                     <div class="container-fluid d-grid gap-1 mt-1">
-                        <a class="btn btn-secondary shadow-sm shadow-sm">Ver subcategorias</a>
-                        <a class="btn btn-secondary shadow-sm">Crear subcategorias</a>
-                        <a class="btn btn-secondary shadow-sm">Eliminar subcategorias</a>
+                        <a class="btn btn btn-outline-secondary shadow-sm">Ver subcategorias</a>
+                        <a class="btn btn btn-outline-secondary shadow-sm">Crear subcategorias</a>
+                        <a class="btn btn btn-outline-secondary shadow-sm">Eliminar subcategorias</a>
                         <hr>
                     </div>
                 </div>
@@ -99,16 +96,16 @@
             <!-- USUARIOS -->
             <div class="row text-center">
                 <div class="col d-grid gap-1">
-                    <button class="btn btn-outline-light shadow-sm" type="button" data-bs-toggle="collapse" data-bs-target="#desplegar-perfil" aria-expanded="false" aria-controls="desplegar-perfil">Perfil</button>
+                    <button class="btn btn-light shadow-sm" type="button" data-bs-toggle="collapse" data-bs-target="#desplegar-perfil" aria-expanded="false" aria-controls="desplegar-perfil">· Perfil ·</button>
                 </div>
                 <div class="collapse" id="desplegar-perfil">
                     <div class="container-fluid d-grid gap-1 mt-1">
-                        <a class="btn btn-secondary shadow-sm">Ver perfil</a>
+                        <a class="btn btn btn-outline-secondary shadow-sm">Editar perfil</a>
+                        <a class="btn btn-outline-danger shadow-sm" href="./logout.php">Cerrar sesión</a>
                         <hr>
                     </div>
                 </div>
             </div>
-
             <!-- USUARIO -->
             </div>
             <?php }; ?>
