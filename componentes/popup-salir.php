dce7483dc6337c1624325507f2378094764f9bd7
Klau <klaudiusmiskinis@gmail.com>
2021-10-27 20:04:44 +0200

    Añadiendo estilo a Pop-Up salir

--- componentes/popup-salir.php
+++ componentes/popup-salir.php
@@ -2,15 +2,14 @@
     <div class="modal fade" id="popup-salir" tabindex="-1">
         <div class="modal-dialog">
             <div class="modal-content bg-dark text-light">
-                <div class="modal-header justify-content-center">
+                <div class="modal-header border-bottom border-light text-center justify-content-center">
                     <h5 class="modal-title">Cerrar sesión</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="modal-body justify-content-center">
+                <div class="modal-body border-bottom border-light text-center">
                     <h6>¿Estás seguro de querer cerrar sesión?</h6>
                 </div>
-                <div class="modal-footer justify-content-center">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                <div class="modal-footer border-0 justify-content-center">
+                    <button type="button" class="btn btn-outline-light" data-bs-dismiss="modal">Cancelar</button>
                     <a type="button" class="btn btn-outline-danger" href="./logout.php">Salir</a>
                 </div>
             </div>
