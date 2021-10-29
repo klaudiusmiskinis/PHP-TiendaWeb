d4921f093267dcae5e7b9ff9881019d01672b68e
Klau <klaudiusmiskinis@gmail.com>
2021-10-29 17:26:27 +0200

    Añadiendo clases y diferentes estilos

--- scripts/ordenar.js
+++ scripts/ordenar.js
@@ -1,3 +1,12 @@
-$(document).ready(function () {
+$(document).ready(function() {
     $('#usuarios').DataTable();
+    $('#usuarios_filter').addClass('col-sm');
+    $('#usuarios_length').addClass('col-sm');
+    $('#usuarios_info').addClass('m-2');
+    $('#usuarios_wrapper').addClass('p-2')
+    $('#usuarios_previous').addClass('btn btn-light');
+    $('#usuarios_next').addClass('btn btn-light');
+    $('input[type="search"]').addClass('form-control');
+    $('select').addClass('form-select');
+    $('.paginate_button').addClass('mx-2');
 });
