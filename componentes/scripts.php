3e41b1a5f61593b45618292d53803774e4571527
Klau <klaudiusmiskinis@gmail.com>
2021-10-29 17:27:11 +0200

    Añadiendo dataTable.min

--- componentes/scripts.php
+++ componentes/scripts.php
@@ -2,7 +2,7 @@
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js"></script>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script type="text/javascript" src="https://cdn.datatables.net/v/bs5/dt-1.11.3/datatables.min.js"></script>
+    <script type="text/javascript" src="../scripts/dataTable.min.js"></script>
     <script type="text/javascript" src="../scripts/urlParameters.js"></script>
     <script type="text/javascript" src="../scripts/animaciones.js"></script>
     <script type="text/javascript" src="../scripts/ordenar.js"></script>
