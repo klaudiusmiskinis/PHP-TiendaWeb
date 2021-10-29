f9323e3ab4c58a781ff360f0b18510cfdc370316
Klau <klaudiusmiskinis@gmail.com>
2021-10-29 19:21:03 +0200

    Añadiendo clases y estilos

--- scripts/ordenar.js
+++ scripts/ordenar.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
     $('#usuarios').DataTable();
-    $('#usuarios_filter').addClass('col-sm');
-    $('#usuarios_length').addClass('col-sm');
-    $('#usuarios_info').addClass('m-2');
     $('#usuarios_wrapper').addClass('p-2')
-    $('#usuarios_previous').addClass('btn btn-light');
+    $('#usuarios_info').addClass('text-center');
     $('#usuarios_next').addClass('btn btn-light');
+    $('#usuarios_previous').addClass('btn btn-light');
     $('input[type="search"]').addClass('form-control');
-    $('select').addClass('form-select');
     $('.paginate_button').addClass('mx-2');
+    $('select').addClass('form-select');
+    $('#usuarios_paginate').addClass('mt-2 text-center');
+}, function() {
+    document.getElementById("usuarios_wrapper").insertAdjacentHTML("beforestart", "<div id='usuarios_cabecera' class='row bg-danger'>");
+    document.getElementById("usuarios_filter").insertAdjacentHTML("afterend", "</div>");
 });
