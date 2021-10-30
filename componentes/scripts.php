b5cb3ab929d72e0aec7ee5aa9d9975a7465f50d6
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-30 15:46:02 +0200

    Cambios en los nombres de scripts

--- componentes/scripts.php
+++ componentes/scripts.php
@@ -2,8 +2,8 @@
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js"></script>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script type="text/javascript" src="../scripts/dataTable.min.js"></script>
-    <script type="text/javascript" src="../scripts/urlParameters.js"></script>
-    <script type="text/javascript" src="../scripts/animaciones.js"></script>
+    <script type="text/javascript" src="../scripts/table.min.js"></script>
+    <script type="text/javascript" src="../scripts/parametros.js"></script>
+    <script type="text/javascript" src="../scripts/animacion.js"></script>
     <script type="text/javascript" src="../scripts/ordenar.js"></script>
 <?php } ?> 
