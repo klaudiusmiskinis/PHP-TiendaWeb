9d2d20cfe6222bd70f2d168c5191fd0eb0218645
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-30 21:12:24 +0200

    Cambiando clases

--- scripts/ordenar.js
+++ scripts/ordenar.js
@@ -5,11 +5,12 @@ $(document).ready(function() {
 
 function estiloDataTable() {
     $('#usuarios_wrapper').addClass('p-2');
-    $('#usuarios_length').addClass('d-flex mb-2')
-    $('#usuarios_filter').addClass('d-flex mb-2')
+    $('#usuarios_length').hide();
+    $('#usuarios_filter').addClass('d-flex mb-3')
     $('#usuarios_info').addClass('text-center');
-    $('#usuarios_next').addClass('btn btn-light');
-    $('#usuarios_previous').addClass('btn btn-light');
+    $('.next').addClass('btn btn-light');
+    $('.previous').addClass('btn btn-light');
+    $('.current').addClass('mx-2');
     $('#usuarios_paginate').addClass('mt-2 text-center');
     $('input[type="search"]').addClass('form-control');
     $('.paginate_button').addClass('mx-2');
