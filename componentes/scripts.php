5e2c08f9de2b40e736ab1b02c0fed50f3534d673
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-30 15:58:24 +0200

    Cambiando rutas CDN por modulos locales

--- componentes/scripts.php
+++ componentes/scripts.php
@@ -1,7 +1,7 @@
 <?php function componenteScripts() { ?>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js"></script>
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script type="text/javascript" src="../node_modules/@popperjs/core/dist/umd/popper.min.js"></script>
+    <script type="text/javascript" src="../node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="../node_modules/jquery/dist/jquery.min.js"></script>
     <script type="text/javascript" src="../scripts/table.min.js"></script>
     <script type="text/javascript" src="../scripts/parametros.js"></script>
     <script type="text/javascript" src="../scripts/animacion.js"></script>
