4a869e61f1eace41298f1350f2009e5dc2dcc9f9
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-30 23:54:58 +0200

    Ordenando y eliminando addClass

--- scripts/ordenar.js
+++ scripts/ordenar.js
@@ -4,15 +4,12 @@ $(document).ready(function() {
 });
 
 function estiloDataTable() {
-    $('#usuarios_wrapper').addClass('p-2');
     $('#usuarios_length').hide();
-    $('#usuarios_filter').addClass('d-flex mb-3')
-    $('#usuarios_info').addClass('text-center');
-    $('.next').addClass('btn btn-light');
-    $('.previous').addClass('btn btn-light');
     $('.current').addClass('mx-2');
+    $('.paginate_button').addClass('mx-2');
+    $('#usuarios_wrapper').addClass('p-2');
+    $('#usuarios_info').addClass('text-center');
+    $('#usuarios_filter').addClass('d-flex mb-3')
     $('#usuarios_paginate').addClass('mt-2 text-center');
     $('input[type="search"]').addClass('form-control');
-    $('.paginate_button').addClass('mx-2');
-    $('select').addClass('form-select');
 }
