5074bf2cf9df4cd12f18c1cce6d23ffcce7af3fc
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-10-31 18:21:47 +0100

    añadiendo clase para margin-bottom

--- scripts/ordenar.js
+++ scripts/ordenar.js
@@ -9,7 +9,7 @@ function estiloDataTable() {
     $('.paginate_button').addClass('mx-2');
     $('#usuarios_wrapper').addClass('p-2');
     $('#usuarios_info').addClass('text-center');
-    $('#usuarios_filter').addClass('d-flex mb-3')
+    $('#usuarios_filter').addClass('mb-3 d-flex')
     $('#usuarios_paginate').addClass('mt-2 text-center');
     $('input[type="search"]').addClass('form-control');
 }
