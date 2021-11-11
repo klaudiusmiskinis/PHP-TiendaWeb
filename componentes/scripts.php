55eb75faced0364334cbbacd5dcb0dfa8ccbb231
Klau <klaudiusmiskinis@gmail.com>
2021-11-11 19:40:03 +0100

    Cambiando rutas de los scripts a local sin usar CDN

--- componentes/scripts.php
+++ componentes/scripts.php
@@ -1,8 +1,8 @@
 <?php function componenteScripts() { ?>
     <script type="text/javascript" src="../node_modules/@popperjs/core/dist/umd/popper.min.js"></script>
     <script type="text/javascript" src="../node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="../node_modules/jquery/dist/jquery.min.js"></script>
-    <script type="text/javascript" src="../scripts/table.min.js"></script>
+    <script src="../node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="../scripts/jquery.dataTables.min.js"></script>
     <script type="text/javascript" src="../scripts/parametros.js"></script>
     <script type="text/javascript" src="../scripts/animacion.js"></script>
     <script type="text/javascript" src="../scripts/ordenar.js"></script>
