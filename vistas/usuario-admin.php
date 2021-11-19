141f0b4c432d913b081ead867a2641b547707354
Klau <klaudiusmiskinis@gmail.com>
2021-11-19 19:31:01 +0100

    Creando botones con accion en modal de formularios

--- vistas/usuario-admin.php
+++ vistas/usuario-admin.php
@@ -18,12 +18,17 @@
     
     <!-- TABLA PARA TODOS LOS USUARIOS -->
     <div class="container-fluid">
-        <div class="row bg-dark text-light rounded p-2">
-            <h4 class="px-2">Todos los usuarios</h4>
-            <?php tablaUsuarios(); ?> 
+        <div class="row bg-dark rounded my-1">
+            <div class="col p-2 d-grid gap-2">
+                <button class="btn btn-outline-light" data-bs-toggle="modal" data-bs-target="#formModalCrearUsuario">Crear un usuario</button>
+                <button class="btn btn-outline-light" data-bs-toggle="modal" data-bs-target="#formModalEditarUsuario">Editar un usuario</button>
+                <button class="btn btn-outline-light" data-bs-toggle="modal" data-bs-target="#formModalEliminarUsuario">Eliminar un usuario</button>
+                <button class="btn btn-outline-light" data-bs-toggle="modal" data-bs-target="#formModalTodosUsuarios">Ver todos los usuarios</button>
+            </div>
         </div>
     </div>
-
+    <?php formModalCrearUsuario(); ?>
+    <?php formModalTodosUsuarios(); ?>
     <?php popupCerrar(); ?>
     <?php componentePanel($_SESSION['nombre'], $_SESSION['rol']); ?>
     <?php componenteScripts(); ?>
