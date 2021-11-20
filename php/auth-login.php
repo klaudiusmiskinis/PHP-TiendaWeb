e4380b76693743918b8395dacf7c2b3752a6aaad
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-11-21 00:49:03 +0100

    Incluyendo clase usuario, estructurando el proyecto para POO. Utilizando metodos para la auteticación de la BBDD y comparado de contraseña encriptado

--- php/auth-login.php
+++ php/auth-login.php
@@ -1,31 +1,29 @@
 <?php
     include("../database/conexion.php");
+    include("../clases/usuario.php");
     session_start();
+    $usuario;
+    $url;
 
     if($_SERVER["REQUEST_METHOD"] == "POST") {
-        $url = '../vistas/home.php';
         $email = $_POST['email'];
         $password = $_POST['password']; 
-
-        //PARA PREVENIR DE INYECCIÓN SQL
         $email = stripcslashes($email);  
         $password = stripcslashes($password);  
-        $email = mysqli_real_escape_string($con, $email);  
-        $password = mysqli_real_escape_string($con, $password);
+        $email = mysqli_real_escape_string($conexion -> getConexion(), $email);  
+        $password = mysqli_real_escape_string($conexion -> getConexion(), $password);
+        
 
-        $sql = "SELECT * FROM usuario WHERE  (email = '$email') AND (password = '$password')";
-        $result = $con->query($sql);
-        if ($result->num_rows > 0) {
-            while($row = $result->fetch_assoc()) {
-                $_SESSION['id'] = $row['id'];
-                $_SESSION['nombre'] = $row['nombre'];
-                $_SESSION['email'] = $row['email'];
-                $_SESSION['rol'] = $row['rol'];
-            }
+        $resultado = $conexion -> authLogin($email);
+        $row = $resultado -> fetch_assoc();
+        
+        if (password_verify($password, $row['password'])) {
+            $url = '../vistas/home.php';
+            $usuario = new Usuario($row['id'], $row['nombre'], $row['email'], $row['password'], $row['avatar'], $row['rol']);
+            $_SESSION['usuario'] = $usuario;
         } else {
-            $url = '../vistas/index.php?error=incorrecto';
+            $url = '../vistas/index.php?error=incorrecto';          
         }
-        $con->close();
         header('Location: '.$url);
     } 
 ?>
