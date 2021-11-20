0b92ec50df5ee136e6e76a602c971944f2de6e44
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-11-20 21:29:46 +0100

    Creando constructor de SQL

--- clases/sql.php
+++ clases/sql.php
@@ -4,6 +4,15 @@
         private $usuario;
         private $password;
         private $database;
-        private $con;
+        private $conexion;
+
+        public function __construct($servidor, $usuario, $password, $database, $conexion)
+        {
+            $this -> servidor = $servidor;
+            $this -> usuario = $usuario;
+            $this -> password = $password;
+            $this -> database = $database;
+            $this -> conexion = $conexion;
+        }
     }
 ?>
