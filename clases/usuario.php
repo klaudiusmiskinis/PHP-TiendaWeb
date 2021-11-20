b22859d934615d73fdd8370783e61e61a3540523
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-11-20 21:03:43 +0100

    Creando atributos, constructor, metodos y getters

--- clases/usuario.php
+++ clases/usuario.php
@@ -1,5 +1,56 @@
 <?php 
     class Usuario {
+        //ATRIBUTOS
+        private $id;
+        private $nombre;
+        private $email;
+        private $password;
+        private $avatar;
+        private $rol;
+
+        //CONSTRUCTOR
+        public function __construct($id, $nombre, $email, $password, $avatar, $rol) {
+            $this -> $id = $id;
+            $this -> $nombre = $nombre;
+            $this -> $email = $nombre;
+            $this -> $password = $password;
+            $this -> $avatar = $avatar;
+            $this -> $rol = $rol;
+        }
+
+        //MÉTODOS
+        /* GETTERs */
+        public function getId() 
+        {
+            return $this -> $id;
+        }
+
+        public function getNombre() 
+        {
+            return $this -> $nombre;
+        }
+       
+        public function getEmail() 
+        {
+            return  $this -> $email;
+        }
+
+        public function getPassword() 
+        {
+            return $this -> $password;
+        }
+
+        public function getAvatar() 
+        {
+            return $this -> $avatar;
+        }
+
+        public function getRol() 
+        {
+            return  $this -> $rol;
+        }
+
+        /* SETTERs */
         
     }
 ?>
