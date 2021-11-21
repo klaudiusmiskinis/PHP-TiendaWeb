16d918a5cd5d0fc06e9c17443d2f993cdbc9ca08
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-11-21 02:05:33 +0100

    Cambiando el session por metodos de la clase usuario

--- vistas/home.php
+++ vistas/home.php
@@ -1,26 +1,27 @@
 <?php
     session_start();
-    if (empty($_SESSION['nombre'])) {
+    if (empty($_SESSION['id'])) {
         header("Location: index.php");
-    }
+   }
+    include('../clases/usuario.php');
     include('../componentes/background.php');
     include('../componentes/scripts.php');
     include('../componentes/navbar.php');
     include('../componentes/head.php');
     include('../componentes/panel-lateral.php');
     include('../componentes/popups.php');
+    $usuario = new Usuario($_SESSION['id'], $_SESSION['nombre'], $_SESSION['email'], $_SESSION['password'], $_SESSION['avatar'], $_SESSION['rol']);
     componenteHead('Home');
-    componenteBackground();
 ?>
 <body>
-<?php componenteNavbar($_SESSION['nombre']); ?>
+<?php componenteBackground(); ?>
+<?php componenteNavbar($usuario -> getNombre()); ?>
 <div class="container-fluid">
     <div class="row text-center rounded p-2">
         <div class="col">Prueba</div>
     </div>
 </div>
-<?php componentePanel($_SESSION['nombre'], $_SESSION['rol']); ?>
+<?php componentePanel($usuario -> getNombre(), $usuario -> getRol()); ?>
 <?php componenteScripts(); ?>
 <?php popupCerrar(); ?>
 </body>
-
