c92506a91e53bb5d7da5efce1fb2e601ed045884
Klau <klaudiusmiskinis@gmail.com>
2021-11-22 17:50:15 +0100

    Cambiando color de los textos en jquery.dataTables

--- scripts/ordenar.js
+++ scripts/ordenar.js
@@ -3,4 +3,7 @@ $(document).ready( function () {
     $('input[type=search]').addClass('mb-1');
     $('input').addClass('bg-light');
     $('select').addClass('bg-light');
+    $('.dataTables_info').addClass('text-light');
+    $('#usuarios_filter').addClass('text-light');
+    $('#usuarios_length').addClass('text-light');
 });
