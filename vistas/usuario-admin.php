37e83e7c8526e5c1079ae6630c88d184aad18449
Klau <klaudiusmiskinis@gmail.com>
2021-11-22 17:51:23 +0100

    Cambiando los formularios y popups por modal

--- vistas/usuario-admin.php
+++ vistas/usuario-admin.php
@@ -7,14 +7,21 @@
     include('../componentes/scripts.php');
     include('../componentes/navbar.php');
     include('../componentes/head.php');
+    include('../componentes/formularios.php');
     include('../componentes/panel-lateral.php');
-    include('../componentes/popups.php');
+    include('../componentes/modal.php');
     include('../componentes/tabla.php');
     componenteHead('ADMIN | Usuario');
     $usuario = new Usuario($_SESSION['id'], $_SESSION['nombre'], $_SESSION['email'], $_SESSION['password'], $_SESSION['avatar'], $_SESSION['rol']);
 ?>
 <body>
     <?php componenteNavbar($usuario -> getNombre()); ?>
+
+    <div class="container-fluid">
+        <div class="row bg-dark rounded my-1">
+            <?php tablaUsuarios(); ?>
+        </div>
+    </div>
 
     <div class="container-fluid">
         <div class="row bg-dark rounded my-1">
@@ -26,10 +33,9 @@
             </div>
         </div>
     </div>
-
+    
+    <?php modalCerrar(); ?>
     <?php formModalCrearUsuario(); ?>
-    <?php formModalTodosUsuarios(); ?>
-    <?php popupCerrar(); ?>
     <?php componentePanel($usuario -> getNombre(), $usuario -> getRol()); ?>
     <?php componenteScripts(); ?>
 </body>
