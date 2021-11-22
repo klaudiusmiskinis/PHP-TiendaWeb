6ffd9cf50a627d9dd1a273554e56d6a775cf564c
Klau <klaudiusmiskinis@gmail.com>
2021-11-22 19:17:25 +0100

    Optimizando código para tener menos líneas

--- php/insert-register.php
+++ php/insert-register.php
@@ -2,15 +2,10 @@
     include("../database/conexion.php");
 
     if($_SERVER["REQUEST_METHOD"] == "POST") {
-        $url = '../vistas/index.php';
-        $nombre = $_POST['nombre'];
-        $email = $_POST['email'];
-        $password = $_POST['password']; 
         $email = strtolower($email);
-
-        $nombre = $conexion -> prevenirInyeccion($nombre);
-        $email = $conexion -> prevenirInyeccion($email);
-        $password = $conexion -> prevenirInyeccion(mysqli_real_escape_string($conexion -> getConexion(), password_hash($password, PASSWORD_BCRYPT, [10])));
+        $nombre = $conexion -> prevenirInyeccion($_POST['nombre']);
+        $email = $conexion -> prevenirInyeccion(strtolower($_POST['email']));
+        $password = $conexion -> prevenirInyeccion(mysqli_real_escape_string($conexion -> getConexion(), password_hash($_POST['password'], PASSWORD_BCRYPT, [10])));
         $url = $conexion -> insertRegister($nombre, $email, $password);
         header('Location: '.$url);
     } 
