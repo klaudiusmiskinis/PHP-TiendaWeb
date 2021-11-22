35196dbad039f871fcc27ecdc8c363a9e4c82343
Klau <klaudiusmiskinis@gmail.com>
2021-11-22 19:17:37 +0100

    Optimizando código para tener menos lineas

--- php/insert-usuario.php
+++ php/insert-usuario.php
@@ -2,15 +2,12 @@
     include("../database/conexion.php");
 
     if($_SERVER["REQUEST_METHOD"] == "POST") {
-        $nombre = $_POST['nombre'];
-        $email = $_POST['email'];
-        $password = $_POST['password']; 
-        $rol = $_POST['rol'];
-        $avatar = $_POST['avatar'];
-        $password = stripcslashes($password);  
-        $email = mysqli_real_escape_string($conexion -> getConexion(), $email);  
-        $password = mysqli_real_escape_string($conexion -> getConexion(), $password);
-        $url = $conexion -> authLogin($email, $password);
+        $nombre = $conexion -> prevenirInyeccion($_POST['nombre']);
+        $email = $conexion -> prevenirInyeccion(strtolower($_POST['email']));
+        $password = $conexion -> prevenirInyeccion(mysqli_real_escape_string($conexion -> getConexion(), password_hash($_POST['password'], PASSWORD_BCRYPT, [10])));
+        $rol = $conexion -> prevenirInyeccion($_POST['rol']);
+        $avatar = file_get_contents($_FILES['avatar']['tmp_name']);
+        $avatar = 'data:image/;base64,'.base64_encode($avatar);
+        $url = $conexion -> insertUsuario($nombre, $email, $password, $rol, $avatar);
         header('Location: '.$url);
-    } 
-?>
+} ?>
