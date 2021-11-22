2364c69c80d3d2ee602888d6cb4754e9a95daf31
Klau <klaudiusmiskinis@gmail.com>
2021-11-22 17:48:46 +0100

    Eliminando type de los scripts

--- componentes/scripts.php
+++ componentes/scripts.php
@@ -1,9 +1,9 @@
 <?php function componenteScripts() { ?>
-    <script type="text/javascript" src="../node_modules/@popperjs/core/dist/umd/popper.min.js"></script>
-    <script type="text/javascript" src="../node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="../node_modules/@popperjs/core/dist/umd/popper.min.js"></script>
+    <script src="../node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
     <script src="../node_modules/jquery/dist/jquery.min.js"></script>
     <script src="../scripts/jquery.dataTables.min.js"></script>
-    <script type="text/javascript" src="../scripts/parametros.js"></script>
-    <script type="text/javascript" src="../scripts/animacion.js"></script>
-    <script type="text/javascript" src="../scripts/ordenar.js"></script>
+    <script src="../scripts/parametros.js"></script>
+    <script src="../scripts/animacion.js"></script>
+    <script src="../scripts/ordenar.js"></script>
 <?php } ?> 
