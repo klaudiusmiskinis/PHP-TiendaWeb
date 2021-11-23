64e7837aae28dbdd898ad1f4aa977aa82c715212
Klau <klaudiusmiskinis@gmail.com>
2021-11-23 20:02:10 +0100

    Creando estilo de la pagina modificar-usuario

--- vistas/modificar-usuario.php
+++ vistas/modificar-usuario.php
@@ -4,12 +4,25 @@
     if ($_SESSION['rol'] != 'admin') {
         header("Location: index.php");
     }
+
+    include('../clases/usuario.php');
+    include('../database/conexion.php');
     include('../componentes/scripts.php');
     include('../componentes/navbar.php');
     include('../componentes/head.php');
+    include('../componentes/panel-lateral.php');
+    include('../componentes/formularios.php');
+    $usuario = new Usuario($_SESSION['id'], $_SESSION['nombre'], $_SESSION['email'], $_SESSION['password'], $_SESSION['avatar'], $_SESSION['rol']);
     $id = $_GET['id'];
+    if (empty($id)){
+        header("Location: index.php");
+    }
+
+    componenteHead('ADMIN | Modificar');
 ?>
 <body>
+    <?php componenteNavbar($usuario -> getNombre()); ?>
     <?php componentePanel($usuario -> getNombre(), $usuario -> getRol()); ?>
+    <?php formModificarUsuario($conexion -> selectUserById($id)); ?>
     <?php componenteScripts(); ?>
 </body>
