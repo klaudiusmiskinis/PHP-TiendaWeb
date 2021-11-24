564b50f297f84eb900663c3477752725522d06c6
Klau <klaudiusmiskinis@gmail.com>
2021-11-24 17:57:26 +0100

    Añadiendo modal a los includes y distintos componentes

--- vistas/modificar-usuario.php
+++ vistas/modificar-usuario.php
@@ -12,6 +12,7 @@
     include('../componentes/head.php');
     include('../componentes/panel-lateral.php');
     include('../componentes/formularios.php');
+    include('../componentes/modal.php');
     $usuario = new Usuario($_SESSION['id'], $_SESSION['nombre'], $_SESSION['email'], $_SESSION['password'], $_SESSION['avatar'], $_SESSION['rol']);
     $id = $_GET['id'];
     if (empty($id)){
@@ -24,5 +25,7 @@
     <?php componenteNavbar($usuario -> getNombre()); ?>
     <?php componentePanel($usuario -> getNombre(), $usuario -> getRol()); ?>
     <?php formModificarUsuario($conexion -> selectUserById($id)); ?>
+    <?php modalCerrar(); ?>
+    <?php modalConfirmarEliminarUsuario($conexion -> selectUserById($id)); ?>
     <?php componenteScripts(); ?>
 </body>
