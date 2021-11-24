a79752ef0752190cca6b9b0bb42df53fef434154
Klau <klaudiusmiskinis@gmail.com>
2021-11-24 17:57:06 +0100

    Creando archivo encargado de eliminar usuario

---
+++ php/delete-usuario.php
@@ -0,0 +1,8 @@
+<?php
+    include("../database/conexion.php");
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        $id = $_POST['idDelete'];
+        $url = $conexion -> deleteUsuario($id);
+        header('Location: '.$url);
+    } 
+?>
