1f9ba278d9a1970b04eac5a2747627d007e1efde
Klau <klaudiusmiskinis@gmail.com>
2021-11-24 20:07:27 +0100

    Eliminando bg-dark por bg-color entre otras clases para el estilo

--- componentes/formularios.php
+++ componentes/formularios.php
@@ -1,9 +1,9 @@
 <?php function formModalCrearUsuario() { ?>
     <div class="modal fade" id="formModalCrearUsuario" tabindex="-1">
         <div class="modal-dialog modal-lg">
-            <div class="modal-content bg-dark">
+            <div class="modal-content bg-color">
                 <div class="modal-header border-bottom border-light text-center justify-content-center">
-                    <h5 class="modal-title text-light">Crear un nuevo usuario</h5>
+                    <h5 class="modal-title text-dark">Crear un nuevo usuario</h5>
                     <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body border-bottom border-light text-center">
@@ -43,39 +43,39 @@
 
 <?php function formCreateCategoria() { ?>
     <div class="modal fade" id="formModalCrearCategoria" tabindex="-1">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content bg-dark">
-            <div class="modal-header border-bottom border-light text-center justify-content-center">
-                <h5 class="modal-title text-light">Crear una nueva categoria</h5>
-                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body border-bottom border-light text-center">
-                <form action="../php/insert-categoria.php" method="POST" id="crearUsuarioForm">
-                    <div class="form-floating mb-3">
-                        <input type="text" name="nombre-categoria" class="form-control text-dark" autocomplete="off" required>
-                        <label for="nombre">Nombre de la categoria</label>
-                    </div>
-                </form>
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content bg-color">
+                <div class="modal-header border-bottom border-light text-center justify-content-center">
+                    <h5 class="modal-title text-dark">Crear una nueva categoria</h5>
+                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body border-bottom border-light text-center">
+                    <form action="../php/insert-categoria.php" method="POST" id="crearUsuarioForm">
+                        <div class="form-floating mb-3">
+                            <input type="text" minlength="4" maxlength="70" name="nombre-categoria" class="form-control text-dark" autocomplete="off" required>
+                            <label for="nombre">Nombre de la categoria</label>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
     </div>
 <?php } ?>
 
 <?php function formModificarUsuario($usuario) { ?>
     <div class="container-fluid">
-        <div class="row bg-dark rounded p-2">
-            <h5 class="text-light m-2">
+        <div class="row bg-color rounded p-2">
+            <h5 class="text-dark m-2">
                 Formulario de actualización
             </h5>
             <form action="../php/update-usuario.php" method="POST">
                 <input type="hidden" name="id" value="<?php echo $usuario['id']; ?>">
                 <div class="form-floating m-2">
-                    <input type="text" name="nombre" class="form-control text-dark" minlength="4" value="<?php echo $usuario['nombre']; ?>" autocomplete="off" required>
+                    <input type="text" name="nombre" class="form-control text-dark" minlength="4" maxlength="35" value="<?php echo $usuario['nombre']; ?>" autocomplete="off" required>
                     <label for="nombre">Nombre del nuevo usuario</label>
                 </div>
                 <div class="form-floating m-2">
-                    <input type="email" name="email" class="form-control text-dark" minlength="7" value="<?php echo $usuario['email']; ?>" autocomplete="off" required>
+                    <input type="email" name="email" class="form-control text-dark" minlength="7" maxlength="35" value="<?php echo $usuario['email']; ?>" autocomplete="off" required>
                     <label for="email">Email del nuevo usuario</label>
                 </div>
                 <div class="form-floating m-2">
