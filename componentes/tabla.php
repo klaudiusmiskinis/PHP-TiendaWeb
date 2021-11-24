b49d073a48388111ecbcf593ecf4d38e56815a06
Klau <klaudiusmiskinis@gmail.com>
2021-11-24 19:11:56 +0100

    Metiendo todo dentro de la misma funcion de crear tablaUsuario

--- componentes/tabla.php
+++ componentes/tabla.php
@@ -1,4 +1,7 @@
-<?php function tablaUsuarios() { ?>
+<?php 
+
+function tablaUsuarios() { ?>
+<?php include('../database/conexion.php');?>
     <div class="table-responsive text-light px-2 py-1" id="tabla-responsive">
         <table class="table table-hover table-striped bg-light border border-dark display" id="usuarios">
             <thead>
@@ -8,35 +11,28 @@
                     <th class="border-bottom border-end border-dark">Email</th>
                     <th class="border-bottom border-end border-dark">Avatar</th>
                     <th class="border-bottom border-end border-dark">Rol</th>
-                    <th class="border-bottom border-end border-dark text-center">Editar</th>
+                    <th class="border-bottom border-end border-dark text-center">Configurar</th>
                 </tr>
             </thead>
             <tbody>
-                <?php listar(); ?>
+                <?php  
+                    $listado = $conexion -> adminUsuariosSelectAll();
+                    while ($row = mysqli_fetch_array($listado)) { 
+                ?>
+                <tr>
+                    <td><?php echo $row['id']; ?></td>
+                    <td><?php echo $row['nombre']; ?></td>
+                    <td><?php echo $row['email']; ?></td>
+                    <td><?php echo 'img/base64';?></td>
+                    <td><?php echo $row['rol']; ?></td>
+                    <td class="text-center">
+                        <a class="text-dark" href="./modificar-usuario.php?id=<?php echo $row['id']; ?>">
+                            <?php generarIconEngranaje(); ?>
+                        </a>
+                    </td>
+                </tr>
+                <?php } ?>
             </tbody>
         </table>
     </div>
-<?php } 
-
-function listar() {
-    include('../database/conexion.php');
-    $listado = $conexion -> adminUsuariosSelectAll();
-    while ($row = mysqli_fetch_array($listado)) {
-        filas($row);
-    }
-}
-
-function filas($row) { ?>
-    <tr>
-        <td><?php echo $row['id']; ?></td>
-        <td><?php echo $row['nombre']; ?></td>
-        <td><?php echo $row['email']; ?></td>
-        <td><?php echo 'img/base64';?></td>
-        <td><?php echo $row['rol']; ?></td>
-        <td class="text-center">
-            <a class="text-dark" href="./modificar-usuario.php?id=<?php echo $row['id']; ?>">
-                <?php generarIconEngranaje(); ?>
-            </a>
-        </td>
-    </tr>
-<?php } ?> 
+<?php } ?>
