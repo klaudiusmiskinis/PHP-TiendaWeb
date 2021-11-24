78535b52921ade113dcc8d96ba9c12c05da20ac3
Klau <klaudiusmiskinis@gmail.com>
2021-11-24 20:08:04 +0100

    cambiando clases de bg-dark a bg-color

--- componentes/navbar.php
+++ componentes/navbar.php
@@ -6,17 +6,17 @@
 
     <nav class="navbar sticky-top" id="navegador">
         <div class="container-fluid">
-            <div class="bg-dark text-light p-2 rounded">
+            <div class="bg-color text-dark p-2 rounded">
                 <a type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBackdrop" aria-controls="offcanvasWithBackdrop">
                     <?php generarIconPerfil(); ?>
                 </a>
             </div>
-            <div class="d-flex bg-dark text-light p-2 rounded">
+            <div class="d-flex bg-color text-dark p-2 rounded">
                 <a class="nav-link" id="item-productos" href="./ofertas.php"><?php generarIconProductos(); ?></a>
                 <a class="nav-link" id="item-home" href="./home.php"><?php generarIconHome(); ?></a>
                 <a class="nav-link" id="item-ofertas" href="./ofertas.php"><?php generarIconOfertas(); ?></a>
             </div>
-            <div class="bg-dark text-light p-2 rounded">
+            <div class="bg-color text-dark p-2 rounded">
                 <a data-bs-toggle="modal" data-bs-target="#modal-salir">
                    <?php generarIconLogOut(); ?>
                 </a>
