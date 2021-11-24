6c4082ea25d57db772662b370b5c797b01e812b1
Klau <klaudiusmiskinis@gmail.com>
2021-11-24 20:08:33 +0100

    Funcion para generar la tablaCategoria

--- componentes/tabla.php
+++ componentes/tabla.php
@@ -2,7 +2,7 @@
 
 function tablaUsuarios() { ?>
 <?php include('../database/conexion.php');?>
-    <div class="table-responsive text-light px-2 py-1" id="tabla-responsive">
+    <div class="table-responsive text-dark px-2 py-1" id="tabla-responsive">
         <table class="table table-hover table-striped bg-light border border-dark display" id="usuarios">
             <thead>
                 <tr>
@@ -27,6 +27,37 @@ function tablaUsuarios() { ?>
                     <td><?php echo $row['rol']; ?></td>
                     <td class="text-center">
                         <a class="text-dark" href="./modificar-usuario.php?id=<?php echo $row['id']; ?>">
+                            <?php generarIconEngranaje(); ?>
+                        </a>
+                    </td>
+                </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
+<?php } ?>
+
+<?php function tablaCategoria() { ?>
+<?php include('../database/conexion.php'); ?>
+    <div class="table-responsive text-dark px-2 py-1" id="tabla-responsive">
+        <table class="table table-hover table-striped bg-light border border-dark display" id="usuarios">
+            <thead>
+                <tr>
+                    <th class="border-bottom border-end border-dark">ID</th>
+                    <th class="border-bottom border-end border-dark">Nombre</th>
+                    <th class="border-bottom border-end border-dark text-center">Configuración</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php  
+                    $listado = $conexion -> adminCategoriasSelectAll();
+                    while ($resultado = mysqli_fetch_array($listado)) { 
+                ?>
+                <tr>
+                    <td><?php echo $resultado['id']; ?></td>
+                    <td><?php echo $resultado['nombre']; ?></td>
+                    <td class="text-center">
+                        <a class="text-dark" href="./modificar-categoria.php?id=<?php echo $resultado['id']; ?>">
                             <?php generarIconEngranaje(); ?>
                         </a>
                     </td>
