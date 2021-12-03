4990a5a320451520ff41789a80ef992733c24fdc
Klau <klaudiusmiskinis@gmail.com>
2021-12-03 20:01:45 +0100

    Cambiando estilo y clases

--- vistas/index.php
+++ vistas/index.php
@@ -9,13 +9,13 @@
     componenteHead('Login');
 ?>
 
-<body class="bg-gradiente">
+<body class="gradiente">
     <!-- LOGIN -->
     <div class='container-fluid' id='container-login'>
         <div class='row justify-content-center mt-5'>
             <div class='col-sm-6'>
-                <div class='text-center text-light'>
-                    <form id="formLogin" class='card gradiente shadow border-0 m-3 p-5' action="../php/auth-login.php" method="POST">
+                <div class='text-center text-dark'>
+                    <form id="formLogin" class='card bg-gradiente shadow m-3 p-5' action="../php/auth-login.php" method="POST">
                         <h1>Login</h1>
                         <p id="frase-login">¡Introduce tu email y contraseña!</p> 
                         <input class='mt-2 shadow-sm form-control' type='email' name='email' placeholder='Email' autocomplete='off' required> 
@@ -39,7 +39,7 @@
                         <input class='mt-2 shadow-sm form-control' type='text' name='nombre' placeholder='Introduce tu nombre de usuario *' autocomplete='off' required>
                         <input class='mt-3 shadow-sm form-control' type='email' name='email' placeholder='Introduce tu email *' autocomplete='off' required> 
                         <input class='mt-3 shadow-sm form-control' type='password' name='password' placeholder='Introduce tu contraseña *' autocomplete='off' required>
-                        <input class='mt-3 shadow-sm text-center btn btn-light' type='submit' value='Registrarse'>
+                        <input class='mt-3 shadow-sm text-center btn btn-secondary' type='submit' value='Registrarse'>
                         <p class='mt-2'>¿Ya tienes una cuenta?</p>
                         <a type="text" id="btn-to-login">¡Inicia sesión!</a>
                     </form>
