76084e969dbb7c9b3a040c80d1af6218a560c62e
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-05 11:47:21 +0100

    Añadiendo links

--- componentes/head.php
+++ componentes/head.php
@@ -8,12 +8,15 @@
         <link rel="stylesheet" href="../node_modules/bootstrap-icons/font/bootstrap-icons.css">
         <link rel='stylesheet' href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
         <link rel='stylesheet' href="../node_modules/datatables.net-dt/css/jquery.dataTables.min.css">
-        <link rel='stylesheet' href='../assets/css/style-general.css?v=<?php echo time(); ?>">'>
-        <link rel='stylesheet' href='../assets/css/panel-lateral.css?v=<?php echo time(); ?>">'>
-        <link rel='stylesheet' href='../assets/css/scrollbar.css?v=<?php echo time(); ?>">'>
-        <link rel='stylesheet' href='../assets/css/clases.css?v=<?php echo time(); ?>">'>
-        <link rel='stylesheet' href='../assets/css/var.css?v=<?php echo time(); ?>">'>
-        <link rel='icon' href='../assets/img/icono.png?v=<?php echo time(); ?>">'>
+        <link rel='stylesheet' href='../assets/css/style-general.css?v=<?php echo time(); ?>'>
+        <link rel='stylesheet' href='../assets/css/panel-lateral.css?v=<?php echo time(); ?>'>
+        <link rel='stylesheet' href='../assets/css/scrollbar.css?v=<?php echo time(); ?>'>
+        <link rel='stylesheet' href='../assets/css/accordion.css?v=<?php echo time(); ?>'>
+        <link rel='stylesheet' href='../assets/css/clases.css?v=<?php echo time(); ?>'>
+        <link rel='stylesheet' href='../assets/css/input.css?v=<?php echo time(); ?>'>
+        <link rel='stylesheet' href='../assets/css/table.css?v=<?php echo time(); ?>'>
+        <link rel='stylesheet' href='../assets/css/var.css?v=<?php echo time(); ?>'>
+        <link rel='icon' href='../assets/img/icono.png?v=<?php echo time(); ?>'>
         <title><?php echo $titulo ?></title>
     </head>
 <?php } ?>
