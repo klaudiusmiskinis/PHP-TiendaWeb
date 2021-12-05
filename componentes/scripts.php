7ebff21be4660e75387eb267a62d30c2c0ebcb41
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-05 11:47:49 +0100

    Añadiendo parametro de tiempo

--- componentes/scripts.php
+++ componentes/scripts.php
@@ -3,7 +3,7 @@
     <script src="../node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
     <script src="../node_modules/jquery/dist/jquery.min.js"></script>
     <script src="../scripts/jquery.dataTables.min.js"></script>
-    <script src="../scripts/parametros.js"></script>
-    <script src="../scripts/animacion.js"></script>
-    <script src="../scripts/ordenar.js"></script>
+    <script src="../scripts/parametros.js?v=<?php echo time(); ?>"></script>
+    <script src="../scripts/animacion.js?v=<?php echo time(); ?>"></script>
+    <script src="../scripts/ordenar.js?v=<?php echo time(); ?>"></script>
 <?php } ?> 
