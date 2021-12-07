70fc8821d4de114fba035411f1388e839df1c1e5
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-07 11:29:16 +0100

    Añadiendo funcionamiento y event listener en caso de que haya cambios en el html de la tabla

--- scripts/ordenar.js
+++ scripts/ordenar.js
@@ -4,12 +4,22 @@ $(document).ready( function () {
     $('select').addClass('mb-1 form-control');
     $('input').addClass('bg-light');
     $('select').addClass('bg-light');
-    $('.dataTables_info').addClass('text-color');
     $('#usuarios_filter').addClass('text-color');
-    $('#usuarios_length').addClass('text-color');
     $('.paginate_button').addClass('btn btn-color');
     $('.dataTables_paginate').addClass('pagination pagination-sm float-end');
     $('.dataTables_paginate').removeClass('dataTables_paginate paging_simple_numbers');
     $('.dataTables_info').addClass('float-start');
     $('.current').addClass('mx-1');
+    $('#usuarios_length').hide();
+    $('#usuarios_info').hide();
+    $('#usuarios_paginate').show();
+    $('#tabla-responsive').on('DOMSubtreeModified', function(){
+        $('#usuarios_paginate').show();
+        $('.paginate_button').addClass('btn btn-color');
+        $('.dataTables_paginate').addClass('pagination pagination-sm float-end');
+        $('.dataTables_paginate').removeClass('dataTables_paginate paging_simple_numbers');
+        $('.dataTables_info').addClass('float-start');
+        $('.current').addClass('mx-1');
+    });
 });
+
