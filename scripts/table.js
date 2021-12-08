08f96b72d46202322e59182ef942d2a9400ff18b
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-08 23:10:56 +0100

    Cambio de nombre de archivo de ordenar > table

--- scripts/table.js
+++ scripts/table.js
@@ -5,16 +5,18 @@ $(document).ready( function () {
     $('input').addClass('bg-light');
     $('select').addClass('bg-light');
     $('#usuarios_filter').addClass('text-color');
+    $('#categorias_filter').addClass('text-color');
     $('.paginate_button').addClass('btn btn-color');
-    $('.dataTables_paginate').addClass('pagination pagination-sm float-end');
-    $('.dataTables_paginate').removeClass('dataTables_paginate paging_simple_numbers');
     $('.dataTables_info').addClass('float-start');
     $('.current').addClass('mx-1');
     $('#usuarios_length').hide();
     $('#usuarios_info').hide();
+    $('#categorias_length').hide();
+    $('#categorias_info').hide();
     $('#usuarios_paginate').show();
     $('#tabla-responsive').on('DOMSubtreeModified', function(){
         $('#usuarios_paginate').show();
+        $('#categorias_paginate').show();
         $('.paginate_button').addClass('btn btn-color');
         $('.dataTables_paginate').addClass('pagination pagination-sm float-end');
         $('.dataTables_paginate').removeClass('dataTables_paginate paging_simple_numbers');
