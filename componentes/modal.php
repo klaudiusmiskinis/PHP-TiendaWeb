5cc1108c38bd5dd7d26a517ced277f26cd101ee4
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-21 03:04:16 +0100

    Creando modalConfirmar para subcategoria

--- componentes/modal.php
+++ componentes/modal.php
@@ -77,6 +77,34 @@
     </div>
 <?php } ?>
 
+<?php function modalConfirmarEliminarSubcategoria($subcategoria) { ?>
+    <div class="modal fade" id="modal-eliminar-subcategoria" tabindex="-1">
+        <div class="modal-dialog">
+            <div class="modal-content border-0 border-color border-bottom border-3">
+                <div class="modal-header bg-color text-light text-center justify-content-center">
+                    <h5 class="modal-title">Confirmar eliminar usuario</h5>
+                </div>
+                <div class="modal-body text-center">
+                    <h6>¿Estás seguro de querer eliminar <?php echo $subcategoria['nombre'] ?>?</h6>
+                    <div class="col mt-2">
+                        <form class="border-0 justify-content-center" action="../php/delete-subcategoria.php" method="POST">
+                            <input type="hidden" name="idDelete" value="<?php echo $subcategoria['id']; ?>">
+                            <div class="row gap-2">
+                                <div class="col d-grid">
+                                    <button type="submit" class="btn btn-danger">Eliminar</button>
+                                </div>
+                                <div class="col d-grid">
+                                    <button type="button" class="btn btn-color" data-bs-dismiss="modal">Cancelar</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php } ?>
+
 <?php function modalConfirmarEliminarProducto($producto) { ?>
     <div class="modal fade" id="modal-eliminar-producto" tabindex="-1">
         <div class="modal-dialog">
@@ -87,7 +115,7 @@
                 <div class="modal-body text-center">
                     <h6>¿Estás seguro de querer eliminar <?php echo $producto['nombre'] ?>?</h6>
                     <div class="col mt-2">
-                        <form class="border-0 justify-content-center" action="../php/delete-usuario.php" method="POST">
+                        <form class="border-0 justify-content-center" action="../php/delete-producto.php" method="POST">
                             <input type="hidden" name="idDelete" value="<?php echo $producto['id']; ?>">
                             <div class="row gap-2">
                                 <div class="col d-grid">
