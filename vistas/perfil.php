433ec92187f0787080ca0824be1e93b845e7eb29
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-21 19:14:12 +0100

    Funcionamiento de los campos de cambio de imagen y combio de contraseña

--- vistas/perfil.php
+++ vistas/perfil.php
@@ -18,11 +18,12 @@
     <?php componenteNavbar($_SESSION['nombre']); ?>
     <div class="container-fluid">
         <div class="row">
-
             <div class="col-4">
                 <div class="d-grid text-center">
                     <div class="card mt-3 text-center">
-                        <img src="<?php echo $usuario -> getAvatar(); ?>" class="img-fluid" style="width: 5em;" alt="profile picture image">
+                        <div class="text-center m-1">
+                            <img src="<?php echo $usuario -> getAvatar(); ?>" class="img-fluid rounded-circle" style="width: 65px;" alt="profile picture image">
+                        </div>
                         <div class="card-body">
                             <ul class="list-group list-group-flush">
                                 <li class="list-group-item"><?php echo $usuario -> getNombre(); ?></li>
@@ -34,34 +35,45 @@
                 </div>
             </div>
 
-            <div class="col-8">
+            <div class="col-8 mt-3">
                 <div class="row">
                     <form action="nombre-usuario.php" method="POST">
                         <div class="d-grid col-11 mx-auto">
-                        <div class="input-group my-3">
+                        <label class="text-color">Cambio de contraseña</label>
+                        <div class="input-group mb-3">
                                 <input type="text" name="nombre-nuevo" id="nombre-nuevo" class="form-control my-1" placeholder="<?php echo $_SESSION['nombre']; ?>" autocomplete="off" required>
-                                <button type="submit" class="btn btn-color my-1">Cambiar nombre</button>
+                                <button type="submit" name="cambiarNombre" class="btn btn-color my-1">Cambiar nombre</button>
                             </div>
                         </div>
                     </form> 
                 </div>
                 <div class="row">
-                    <form action="password-usuario.php" method="POST">
+                    <form action="../php/update-perfil.php" method="POST">
                         <div class="d-grid col-11 mx-auto">
-                            <input type="password" name="password-one" id="password-one" class="form-control my-1" placeholder="Introduce la nueva contraseña" autocomplete="off" required>
+                            <label class="text-color">Cambio de contraseña</label>
+                            <input type="hidden" name="id" value="<?php echo $usuario -> getId(); ?>">
+                            <input type="password" name="password-one" id="password-one" minlength="4" maxlength="70" class="form-control my-1" placeholder="Introduce la nueva contraseña" autocomplete="off" required>
                             <div class="input-group">
-                                <input type="password" name="password-two" id="password-two" class="form-control my-1" placeholder="Vuelve a introducir tu nueva contraseña" autocomplete="off" required>
-                                <button type="submit" id="form-password-user" class="btn btn-color my-1" >Cambiar contraseña</button>
+                                <input type="password" name="password-two" id="password-two" minlength="4" maxlength="70" class="form-control my-1" placeholder="Vuelve a introducir tu nueva contraseña" autocomplete="off" required>
+                                <button type="submit" name="form-password-user" class="btn btn-color my-1">Cambiar contraseña</button>
                             </div>
+                            <label class="text-color mb-3"><i class="bi bi-info-circle-fill me-2"></i>Las contraseñas deben coincidir y tener mínimo 4 caracteres</label>
                         </div>
                     </form>
                 </div>
                 <div class="row">
                     <form action="../php/update-perfil.php" method="POST" enctype="multipart/form-data">
                         <div class="d-grid col-11 mx-auto">
-                            <div class="input-group my-3">
-                                <input accept="image/png" type="file" name="imagenPerfil" id="imagenPerfil" class="form-control" required>
-                                <button type="submit" name="form-img-user" class="input-group-text btn btn-color">Cambiar imagen</button>
+                            <label class="text-color">Cambio de imagen de perfil</label>
+                            <div class="row">
+                                <div class="col">
+                                    <input accept="image/png, image/jpeg" type="file" name="imagenPerfil" id="imagenPerfil" class="hide" required>
+                                    <label for="imagenPerfil" class="btn btn-color d-grid rounded-0" required>Seleccionar archivo</label>
+                                </div>
+                                <div class="col-auto d-grid mb-1">
+                                    <button type="submit" name="form-img-user" class="input-group-text btn btn-color rounded-0">Cambiar imagen</button>
+                                </div>
+                                <label class="text-color mb-3"><i class="bi bi-info-circle-fill me-2"></i>Solo se aceptan JPG, JPEG y PNG</label>
                             </div>
                         </div>
                     </form>
