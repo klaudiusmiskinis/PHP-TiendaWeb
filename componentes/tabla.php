b8ac47ccee1bf39ee146bed4a45b0a3ab7ee85e0
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-12-21 02:34:31 +0100

    Añadiendo tablaProductos y tablaSubcategoria

--- componentes/tabla.php
+++ componentes/tabla.php
@@ -64,3 +64,81 @@ function tablaUsuarios($conexion) { ?>
         </table>
     </div>
 <?php } ?>
+
+<?php function tablaProducto($conexion) { ?>
+    <div class="table-responsive text-dark" id="tabla-responsive">
+        <table class="table table-hover table-striped bg-light display" id="productos">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Tipo</th>
+                    <th>Peso</th>
+                    <th>Precio</th>
+                    <th>Fecha Introducido</th>
+                    <th>Marca</th>
+                    <th>Imagen</th>
+                    <th>Categoria</th>
+                    <th>idSubcategoria</th>
+                    <th class="text-center">Configuración</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php  
+                    $listado = $conexion -> adminProductosSelectAll();
+                    while ($resultado = mysqli_fetch_array($listado)) { 
+                ?>
+                <tr>
+                    <td><?php echo $resultado['id']; ?></td>
+                    <td><?php echo $resultado['nombre']; ?></td>
+                    <td><?php echo $resultado['tipo']; ?></td>
+                    <td><?php echo $resultado['peso']; ?></td>
+                    <td><?php echo $resultado['precio']; ?></td>
+                    <td><?php echo $resultado['fechaIntroducido']; ?></td>
+                    <td><?php echo $resultado['marca']; ?></td>
+                    <td>file/img</td>
+                    <td><?php echo $resultado['idCategoria']; ?></td>
+                    <td><?php echo $resultado['idSubcategoria']; ?></td>
+                    <td class="text-center">
+                        <a class="text-color text-center" href="./modificar-producto.php?id=<?php echo $resultado['id'];?>">
+                            <i class="bi bi-wrench icono"></i>
+                        </a>
+                    </td>
+                </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
+<?php } ?>
+
+<?php function tablaSubcategoria($conexion) { ?>
+    <div class="table-responsive text-dark" id="tabla-responsive">
+        <table class="table table-hover table-striped bg-light display" id="categorias">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>ID Categoria</th>
+                    <th class="text-center">Configuración</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php  
+                    $listado = $conexion -> adminSubcategoriaSelectAll();
+                    while ($resultado = mysqli_fetch_array($listado)) { 
+                ?>
+                <tr>
+                    <td><?php echo $resultado['id']; ?></td>
+                    <td><?php echo $resultado['nombre']; ?></td>
+                    <td><?php echo $resultado['idCategoria']; ?></td>
+                    <td class="text-center">
+                        <a class="text-color text-center" href="./modificar-subcategoria.php?id=<?php echo $resultado['id']; ?>">
+                            <i class="bi bi-wrench icono"></i>
+                        </a>
+                    </td>
+                </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
+<?php } ?>
